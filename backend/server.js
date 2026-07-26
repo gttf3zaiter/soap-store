fb8c3370579ab408eebb78aac64296e536bc5f37
Ahmad Zaiter <zaiter12lite3@gmail.com>
2026-07-26 15:51:17 +0300

    check database hose

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,9 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
+
+
+console.log("DATABASE HOST:", process.env.DB_HOST);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
